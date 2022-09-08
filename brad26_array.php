6885bb0da3b058fea14e602cffb534413872767f
Hank <hank.tsunghanlin@gmail.com>
2022-09-08 11:58:37 +0800

    2209081158

---
+++ brad26_array.php
@@ -0,0 +1,18 @@
+<?php
+    $i = 1;
+    $p = array(1=>0,0,0,0,0,0);
+    var_dump($p);
+    
+    // while($i++<=100){
+    //     $point = rand(1,6);
+    //     switch($point){
+    //         case 1: $one++; break;
+    //         case 2: $two++; break;
+    //         case 3: $three++; break;
+    //         case 4: $four++; break;
+    //         case 5: $five++; break;
+    //         case 6: $six++; break;
+    //     }
+        
+    // }
+    // echo "{$one},{$two},{$three},{$four},{$five},{$six}";
