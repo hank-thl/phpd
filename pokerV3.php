b58725198ed3b20944a321ee167a64c6b9867d94
Hank <hank.tsunghanlin@gmail.com>
2022-09-12 12:14:00 +0800

    2209121213

---
+++ pokerV3.php
@@ -0,0 +1,20 @@
+<?php
+    $poker = range(0,51);
+    shuffle($poker);
+
+    foreach($poker as $v){
+         echo "{$v} <br>";
+    }
+
+
+    $players = [[],[],[],[]];
+    var_dump($players);
+    echo "<hr>";
+    foreach($poker as $k => $v){
+        $players[$k%4][(int)($k/4)] = $v;
+   }
+   foreach($players[0]as $card){
+    echo"{$card}<br>";
+   }
+
+?>
